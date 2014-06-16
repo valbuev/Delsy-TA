c935a858e255738c21c3f79c1ed9994dd86db2e8
Valeriy MacOS 10.9.2, Delsy, VM on mss76 <valbuev@gmail.com>
2014-06-16 17:35:17 +0800

    Initial Commit

---
+++ Delsy TA/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Delsy TA
+//
+//  Created by Valeriy Buev on 16.06.14.
+//  Copyright (c) 2014 bva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
