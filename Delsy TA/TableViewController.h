d84c7192d7501e407e63347865060fa43e2ff199
Valeriy MacOS 10.9.2, Delsy, VM on mss76 <valbuev@gmail.com>
2014-06-23 16:36:23 +0800

    Qty-getter has been added, and also cap for ClientList has been added

---
+++ Delsy TA/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  Delsy TA
+//
+//  Created by Valeriy Buev on 23.06.14.
+//  Copyright (c) 2014 bva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
