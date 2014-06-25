9927ad28d59ece70d67f3d0d17a3276d6c107b5b
Valeriy MacOS 10.9.2, Delsy, VM on mss76 <valbuev@gmail.com>
2014-06-25 10:00:15 +0800

    first

---
+++ Delsy TA/Updaters/BVAFileDownloader.h
@@ -0,0 +1,13 @@
+//
+//  BVAFileDownloader.h
+//  Delsy TA
+//
+//  Created by Valeriy Buev on 25.06.14.
+//  Copyright (c) 2014 bva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BVAFileDownloader : NSObject
+
+@end
---
+++ Delsy TA/Updaters/BVAFileDownloader.m
@@ -0,0 +1,14 @@
+//
+//  BVAFileDownloader.m
+//  Delsy TA
+//
+//  Created by Valeriy Buev on 25.06.14.
+//  Copyright (c) 2014 bva. All rights reserved.
+//
+
+#import "BVAFileDownloader.h"
+
+@implementation BVAFileDownloader
+<NSURLSessionDelegate, NSURLSessionDataDelegate,NSURLSessionDownloadDelegate>
+
+@end
